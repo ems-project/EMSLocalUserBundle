8cd58b89096751df59403c988627174d2d6f895e
Mathieu De Keyzer <mathieu@dekeyzer.net>
2017-11-30 12:20:45 +0100

    Update publish role user

--- Resources/DoctrineMigrations/pdo_mysql/Version20170829101534.php
+++ Resources/DoctrineMigrations/pdo_mysql/Version20170829101534.php
@@ -17,8 +17,9 @@ class Version20170829101534 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
+        
         $this->addSql('ALTER TABLE content_type ADD publish_role VARCHAR(100) DEFAULT NULL');
+        $this->addSql("UPDATE content_type SET publish_role = 'ROLE_USER'");
     }
 
     /**
