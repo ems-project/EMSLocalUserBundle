24c24ff407b6066fb91618b679b8f048658187da
theus deka <me@theus.be>
2018-01-11 14:06:48 +0100

    Bigint for asset size

---
+++ Resources/DoctrineMigrations/pdo_pgsql/Version20180111130417.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180111130417 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE revision ALTER labelfield TYPE TEXT');
+        $this->addSql('ALTER TABLE revision ALTER labelfield DROP DEFAULT');
+        $this->addSql('ALTER TABLE uploaded_asset ALTER size TYPE BIGINT');
+        $this->addSql('ALTER TABLE uploaded_asset ALTER size DROP DEFAULT');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE revision ALTER labelField TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE revision ALTER labelField DROP DEFAULT');
+        $this->addSql('ALTER TABLE uploaded_asset ALTER size TYPE INT');
+        $this->addSql('ALTER TABLE uploaded_asset ALTER size DROP DEFAULT');
+    }
+}
