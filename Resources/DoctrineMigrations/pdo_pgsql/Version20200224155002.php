aad614dcdc3f6a6c105967c2b476177c6b07c0e5
Jérôme Coppée <j.coppee@gmail.com>
2020-02-25 17:35:18 +0100

    feature: remove CREATE SCHEMA public

--- Resources/DoctrineMigrations/pdo_pgsql/Version20200224155002.php
+++ Resources/DoctrineMigrations/pdo_pgsql/Version20200224155002.php
@@ -24,7 +24,6 @@ final class Version20200224155002 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE content_type DROP searchLinkDisplayRole');
         $this->addSql('ALTER TABLE content_type DROP createLinkDisplayRole');
     }
